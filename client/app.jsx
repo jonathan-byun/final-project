f3337cdac1e9b147ce536da47af8716aae2ac99c
jonathan-byun <jonathanbyun1@gmail.com>
2022-04-13 11:57:39 -0700

    Added parseRoute function

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import Home from './pages/home';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
   render() {
     return <Home />;
   }
