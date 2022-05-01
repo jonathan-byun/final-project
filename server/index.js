f5b2f9e47287a06f7339cb3e9f9f3dc3a776e5ec
jonathan-byun <jonathanbyun1@gmail.com>
2022-05-01 12:01:22 -0700

    removed duplicate code

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 const db = require('./db');
-require('dotenv/config');
 
 const app = express();
 
